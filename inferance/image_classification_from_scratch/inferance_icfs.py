7b395cdc251e8cffa65297f96545f06e1ff8abf8
Shogo Iwai <iwai.shogo@megachips.co.jp>
2020-12-01 17:09:08 +0900

    added imega show function

--- inferance/image_classification_from_scratch/inferance_icfs.py
+++ inferance/image_classification_from_scratch/inferance_icfs.py
@@ -1,15 +1,23 @@
+import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.models import load_model
 
+imgfile = "../../training/image_classification_from_scratch/PetImages/Cat/6779.jpg"
+
+img = plt.imread(imgfile)
+plt.imshow(img)
+plt.show()
+
 image_size = (180, 180)
 img = keras.preprocessing.image.load_img(
-    "../../training/image_classification_from_scratch/PetImages/Cat/6779.jpg", target_size=image_size
+    imgfile, target_size=image_size
 )
 img_array = keras.preprocessing.image.img_to_array(img)
 img_array = tf.expand_dims(img_array, 0)  # Create batch axis
 
-modelfile = '../../training/image_classification_from_scratch/model_of_image_classification_for_dog_and_cat.h5'
+modelfile = '../../training/image_classification_from_scratch/save_at_1.h5'
+# modelfile = '../../training/image_classification_from_scratch/model_of_image_classification_for_dog_and_cat.h5'
 model = load_model(modelfile)
 predictions = model.predict(img_array)
 score = predictions[0]
