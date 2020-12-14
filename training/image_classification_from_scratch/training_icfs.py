22a52ed4d3b3f447814840171e24356be4d808ce
Shogo Iwai <iwai.shogo@megachips.co.jp>
2020-12-14 17:17:43 +0900

    devided software to third about icfs

--- training/image_classification_from_scratch/training_icfs.py
+++ training/image_classification_from_scratch/training_icfs.py
@@ -1,20 +1,30 @@
-"""
-## Introduction
-This example shows how to do image classification from scratch, starting from JPEG
-image files on disk, without leveraging pre-trained weights or a pre-made Keras
-Application model. We demonstrate the workflow on the Kaggle Cats vs Dogs binary
- classification dataset.
-We use the `image_dataset_from_directory` utility to generate the datasets, and
-we use Keras image preprocessing layers for image standardization and data augmentation.
-"""
-
-"""
-## Setup
-"""
-
+from argparse import ArgumentParser
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras import layers
+from tensorflow.keras.models import load_model
+import os
+import sys
+
+global opts
+opts = {}
+
+def parseOptions():
+    argparser = ArgumentParser()
+    argparser.add_argument('--prp', help=':preparing data set', action='store_true') # use action='store_true' as flag
+    argparser.add_argument('--fil', help=':filtering data set', action='store_true') # use action='store_true' as flag
+    argparser.add_argument('--rmn', help=':specify read model name') # use action='store_true' as flag
+    argparser.add_argument('--wmn', help=':specify write model name') # use action='store_true' as flag
+    args = argparser.parse_args()
+    if args.prp: opts.update({'prp':args.prp})
+    if args.fil: opts.update({'fil':args.fil})
+    if args.rmn: opts.update({'rmn':args.rmn})
+    if args.wmn: opts.update({'wmn':args.wmn})
+
+image_size = (180, 180)
+batch_size = 32
+epochs = 50
+
+parseOptions()
 
 """
 ## Load the data: the Cats vs Dogs dataset
@@ -28,7 +38,6 @@ curl -O https://download.microsoft.com/download/3/E/1/3E1C3F21-ECDB-4869-8368-6D
 
 """shell
 unzip -q kagglecatsanddogs_3367a.zip
-ls
 """
 
 """
@@ -36,9 +45,9 @@ Now we have a `PetImages` folder which contain two subfolders, `Cat` and `Dog`. 
  subfolder contains image files for each category.
 """
 
-"""shell
-ls PetImages
-"""
+if ('prp' in opts.keys()):
+    os.system(r'curl -O https://download.microsoft.com/download/3/E/1/3E1C3F21-ECDB-4869-8368-6DEBA77B919F/kagglecatsanddogs_3367a.zip')
+    os.system(r'unzip -q kagglecatsanddogs_3367a.zip')
 
 """
 ### Filter out corrupted images
@@ -47,96 +56,27 @@ occurence. Let's filter out badly-encoded images that do not feature the string 
  in their header.
 """
 
-import os
+if ('fil' in opts.keys()):
+    num_skipped = 0
+    for folder_name in ("Cat", "Dog"):
+        folder_path = os.path.join("PetImages", folder_name)
+        for fname in os.listdir(folder_path):
+            fpath = os.path.join(folder_path, fname)
+            try:
+                fobj = open(fpath, "rb")
+                is_jfif = tf.compat.as_bytes("JFIF") in fobj.peek(10)
+            finally:
+                fobj.close()
 
-num_skipped = 0
-for folder_name in ("Cat", "Dog"):
-    folder_path = os.path.join("PetImages", folder_name)
-    for fname in os.listdir(folder_path):
-        fpath = os.path.join(folder_path, fname)
-        try:
-            fobj = open(fpath, "rb")
-            is_jfif = tf.compat.as_bytes("JFIF") in fobj.peek(10)
-        finally:
-            fobj.close()
+            if not is_jfif:
+                num_skipped += 1
+                # Delete corrupted image
+                os.remove(fpath)
 
-        if not is_jfif:
-            num_skipped += 1
-            # Delete corrupted image
-            os.remove(fpath)
+    print("Deleted %d images" % num_skipped)
 
-print("Deleted %d images" % num_skipped)
-
-"""
-## Generate a `Dataset`
-"""
-
-image_size = (180, 180)
-batch_size = 32
-
-train_ds = tf.keras.preprocessing.image_dataset_from_directory(
-    "PetImages",
-    validation_split=0.2,
-    subset="training",
-    seed=1337,
-    image_size=image_size,
-    batch_size=batch_size,
-)
-val_ds = tf.keras.preprocessing.image_dataset_from_directory(
-    "PetImages",
-    validation_split=0.2,
-    subset="validation",
-    seed=1337,
-    image_size=image_size,
-    batch_size=batch_size,
-)
-
-"""
-## Visualize the data
-Here are the first 9 images in the training dataset. As you can see, label 1 is "dog"
- and label 0 is "cat".
-"""
-
-import matplotlib.pyplot as plt
-
-plt.figure(figsize=(10, 10))
-for images, labels in train_ds.take(1):
-    for i in range(9):
-        ax = plt.subplot(3, 3, i + 1)
-        plt.imshow(images[i].numpy().astype("uint8"))
-        plt.title(int(labels[i]))
-        plt.axis("off")
-# plt.show()
-
-"""
-## Using image data augmentation
-When you don't have a large image dataset, it's a good practice to artificially
-introduce sample diversity by applying random yet realistic transformations to the
-training images, such as random horizontal flipping or small random rotations. This
-helps expose the model to different aspects of the training data while slowing down
- overfitting.
-"""
-
-data_augmentation = keras.Sequential(
-    [
-        layers.experimental.preprocessing.RandomFlip("horizontal"),
-        layers.experimental.preprocessing.RandomRotation(0.1),
-    ]
-)
-
-"""
-Let's visualize what the augmented samples look like, by applying `data_augmentation`
- repeatedly to the first image in the dataset:
-"""
-
-plt.figure(figsize=(10, 10))
-for images, _ in train_ds.take(1):
-    for i in range(9):
-        augmented_images = data_augmentation(images)
-        ax = plt.subplot(3, 3, i + 1)
-        plt.imshow(augmented_images[0].numpy().astype("uint8"))
-        plt.axis("off")
-# plt.show()
+if (not ('rmn' in opts.keys() and 'wmn' in opts.keys())):
+    sys.exit()
 
 """
 ## Standardizing the data
@@ -148,34 +88,23 @@ standardize values to be in the `[0, 1]` by using a `Rescaling` layer at the sta
  our model.
 """
 
-"""
-## Two options to preprocess the data
-There are two ways you could be using the `data_augmentation` preprocessor:
-**Option 1: Make it part of the model**, like this:
-```python
-inputs = keras.Input(shape=input_shape)
-x = data_augmentation(inputs)
-x = layers.experimental.preprocessing.Rescaling(1./255)(x)
-...  # Rest of the model
-```
-With this option, your data augmentation will happen *on device*, synchronously
-with the rest of the model execution, meaning that it will benefit from GPU
- acceleration.
-Note that data augmentation is inactive at test time, so the input samples will only be
- augmented during `fit()`, not when calling `evaluate()` or `predict()`.
-If you're training on GPU, this is the better option.
-**Option 2: apply it to the dataset**, so as to obtain a dataset that yields batches of
- augmented images, like this:
-```python
-augmented_train_ds = train_ds.map(
-  lambda x, y: (data_augmentation(x, training=True), y))
-```
-With this option, your data augmentation will happen **on CPU**, asynchronously, and will
- be buffered before going into the model.
-If you're training on CPU, this is the better option, since it makes data augmentation
- asynchronous and non-blocking.
-In our case, we'll go with the first option.
-"""
+train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    "PetImages",
+    validation_split=0.2,
+    subset="training",
+    seed=1337,
+    image_size=image_size,
+    batch_size=batch_size,
+)
+
+val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    "PetImages",
+    validation_split=0.2,
+    subset="validation",
+    seed=1337,
+    image_size=image_size,
+    batch_size=batch_size,
+)
 
 """
 ## Configure the dataset for performance
@@ -187,85 +116,15 @@ train_ds = train_ds.prefetch(buffer_size=32)
 val_ds = val_ds.prefetch(buffer_size=32)
 
 """
-## Build a model
-We'll build a small version of the Xception network. We haven't particularly tried to
-optimize the architecture; if you want to do a systematic search for the best model
- configuration, consider using
-[Keras Tuner](https://github.com/keras-team/keras-tuner).
-Note that:
-- We start the model with the `data_augmentation` preprocessor, followed by a
- `Rescaling` layer.
-- We include a `Dropout` layer before the final classification layer.
-"""
-
-def make_model(input_shape, num_classes):
-    inputs = keras.Input(shape=input_shape)
-    # Image augmentation block
-    x = data_augmentation(inputs)
-
-    # Entry block
-    x = layers.experimental.preprocessing.Rescaling(1.0 / 255)(x)
-    x = layers.Conv2D(32, 3, strides=2, padding="same")(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.Activation("relu")(x)
-
-    x = layers.Conv2D(64, 3, padding="same")(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.Activation("relu")(x)
-
-    previous_block_activation = x  # Set aside residual
-
-    for size in [128, 256, 512, 728]:
-        x = layers.Activation("relu")(x)
-        x = layers.SeparableConv2D(size, 3, padding="same")(x)
-        x = layers.BatchNormalization()(x)
-
-        x = layers.Activation("relu")(x)
-        x = layers.SeparableConv2D(size, 3, padding="same")(x)
-        x = layers.BatchNormalization()(x)
-
-        x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
-
-        # Project residual
-        residual = layers.Conv2D(size, 1, strides=2, padding="same")(
-            previous_block_activation
-        )
-        x = layers.add([x, residual])  # Add back residual
-        previous_block_activation = x  # Set aside next residual
-
-    x = layers.SeparableConv2D(1024, 3, padding="same")(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.Activation("relu")(x)
-
-    x = layers.GlobalAveragePooling2D()(x)
-    if num_classes == 2:
-        activation = "sigmoid"
-        units = 1
-    else:
-        activation = "softmax"
-        units = num_classes
-
-    x = layers.Dropout(0.5)(x)
-    outputs = layers.Dense(units, activation=activation)(x)
-    return keras.Model(inputs, outputs)
-
-model = make_model(input_shape=image_size + (3,), num_classes=2)
-# keras.utils.plot_model(model, show_shapes=True)
-
-"""
 ## Train the model
 """
-
-epochs = 50
 
 callbacks = [
     keras.callbacks.ModelCheckpoint("save_at_{epoch}.h5"),
 ]
-model.compile(
-    optimizer=keras.optimizers.Adam(1e-3),
-    loss="binary_crossentropy",
-    metrics=["accuracy"],
-)
+
+model = load_model(opts['rmn'])
+model.summary()
 model.fit(
     train_ds, epochs=epochs, callbacks=callbacks, validation_data=val_ds,
 )
@@ -274,5 +133,4 @@ model.fit(
 We get to ~96% validation accuracy after training for 50 epochs on the full dataset.
 """
 
-modelfile = './model_of_image_classification_for_dog_and_cat.h5'
-model.save(modelfile)
+model.save(opts['wmn'])
