118db493254c4eebdac9be669da8e8c0861a9db4
iwai.shogo <zive@miobox.jp>
2020-12-20 17:52:55 +0900

    modified gid option

--- common/gid/gid.py
+++ common/gid/gid.py
@@ -7,21 +7,28 @@ opts = {}
 
 def parseOptions():
     argparser = ArgumentParser()
-    argparser.add_argument('--ctg', help=':specify category') # use action='store_true' as flag
     argparser.add_argument('--kwd', help=':specify keyword') # use action='store_true' as flag
+    argparser.add_argument('--ctg', help=':specify category') # use action='store_true' as flag
+    argparser.add_argument('--lim', help=':optional, specify limit') # use action='store_true' as flag
     args = argparser.parse_args()
     if args.ctg: opts.update({'ctg':args.ctg})
     if args.kwd: opts.update({'kwd':args.kwd})
+    if args.lim: opts.update({'lim':args.lim})
 
-def main(limit='100'):
-    dir = opts['kwd']
-    dir = re.sub('\s', '_', dir)
+def main(limit='10'):
+    outdir = opts['kwd']
+    outdir = re.sub('\s', '_', outdir)
+    imgdir = opts['ctg']
+    imgdir = re.sub('\s', '_', imgdir)
     arguments = {
-        "keywords":f"{opts['ctg']} {opts['kwd']}",
-        "output_directory":f"{dir}",
-        "format":"jpg",
-        "limit":f"{limit}",
-        "print_urls":True,
+        'keywords':f"{opts['kwd']}",
+        'output_directory':f"{outdir}",
+        'prefix_keywords':f"{opts['ctg']}",
+        'image_directory':f"{imgdir}",
+        'limit':f"{limit}",
+        'format':'jpg',
+        'no_numbering':True,
+        'print_urls':True,
     } 
 
     print(arguments)
@@ -29,4 +36,8 @@ def main(limit='100'):
     response.download(arguments)
 
 parseOptions()
-if ('ctg' in opts.keys() and 'kwd' in opts.keys()): main()
+if ('ctg' in opts.keys() and 'kwd' in opts.keys()):
+    if ('lim' in opts.keys()):
+        main(opts['lim'])
+    else:
+        main()
