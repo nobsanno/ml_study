a5967ab53a17af12a14544ae27ce61be3bc90fef
Shogo Iwai <iwai.shogo@megachips.co.jp>
2020-12-30 14:49:45 +0900

    separated common function

---
+++ modeling/image_classification_from_scratch/modeling_icfs_com.py
@@ -0,0 +1,60 @@
+from tensorflow import keras
+from tensorflow.keras import layers
+
+data_augmentation = keras.Sequential(
+    [
+        layers.experimental.preprocessing.RandomFlip("horizontal"),
+        layers.experimental.preprocessing.RandomRotation(0.1),
+    ]
+)
+
+def make_model(input_shape, num_classes, sigen=False):
+    inputs = keras.Input(shape=input_shape)
+    # Image augmentation block
+    x = data_augmentation(inputs)
+
+    # Entry block
+    x = layers.experimental.preprocessing.Rescaling(1.0 / 255)(x)
+    x = layers.Conv2D(32, 3, strides=2, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    x = layers.Conv2D(64, 3, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    previous_block_activation = x  # Set aside residual
+
+    for size in [128, 256, 512, 728]:
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(size, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(size, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
+
+        # Project residual
+        residual = layers.Conv2D(size, 1, strides=2, padding="same")(
+            previous_block_activation
+        )
+        x = layers.add([x, residual])  # Add back residual
+        previous_block_activation = x  # Set aside next residual
+
+    x = layers.SeparableConv2D(1024, 3, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    x = layers.GlobalAveragePooling2D()(x)
+    if (sigen and num_classes == 2):
+        activation = "sigmoid"
+        units = 1
+    else:
+        activation = "softmax"
+        units = num_classes
+    
+    x = layers.Dropout(0.5)(x)
+    outputs = layers.Dense(units, activation=activation)(x)
+    return keras.Model(inputs, outputs)
--- modeling/image_classification_from_scratch/modeling_icfs_sample.py
+++ modeling/image_classification_from_scratch/modeling_icfs_sample.py
@@ -1,7 +1,5 @@
 from argparse import ArgumentParser
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers
+import modeling_icfs_com as mic
 
 global opts
 opts = {}
@@ -16,129 +14,12 @@ def parseOptions():
 
 image_size = (180, 180)
 
-"""
-## Introduction
-This example shows how to do image classification from scratch, starting from JPEG
-image files on disk, without leveraging pre-trained weights or a pre-made Keras
-Application model. We demonstrate the workflow on the Kaggle Cats vs Dogs binary
- classification dataset.
-We use the `image_dataset_from_directory` utility to generate the datasets, and
-we use Keras image preprocessing layers for image standardization and data augmentation.
-"""
-
-"""
-## Using image data augmentation
-When you don't have a large image dataset, it's a good practice to artificially
-introduce sample diversity by applying random yet realistic transformations to the
-training images, such as random horizontal flipping or small random rotations. This
-helps expose the model to different aspects of the training data while slowing down
- overfitting.
-"""
-
-"""
-## Two options to preprocess the data
-There are two ways you could be using the `data_augmentation` preprocessor:
-**Option 1: Make it part of the model**, like this:
-```python
-inputs = keras.Input(shape=input_shape)
-x = data_augmentation(inputs)
-x = layers.experimental.preprocessing.Rescaling(1./255)(x)
-...  # Rest of the model
-```
-With this option, your data augmentation will happen *on device*, synchronously
-with the rest of the model execution, meaning that it will benefit from GPU
- acceleration.
-Note that data augmentation is inactive at test time, so the input samples will only be
- augmented during `fit()`, not when calling `evaluate()` or `predict()`.
-If you're training on GPU, this is the better option.
-**Option 2: apply it to the dataset**, so as to obtain a dataset that yields batches of
- augmented images, like this:
-```python
-augmented_train_ds = train_ds.map(
-  lambda x, y: (data_augmentation(x, training=True), y))
-```
-With this option, your data augmentation will happen **on CPU**, asynchronously, and will
- be buffered before going into the model.
-If you're training on CPU, this is the better option, since it makes data augmentation
- asynchronous and non-blocking.
-In our case, we'll go with the first option.
-"""
-
-data_augmentation = keras.Sequential(
-    [
-        layers.experimental.preprocessing.RandomFlip("horizontal"),
-        layers.experimental.preprocessing.RandomRotation(0.1),
-    ]
-)
-
-"""
-## Build a model
-We'll build a small version of the Xception network. We haven't particularly tried to
-optimize the architecture; if you want to do a systematic search for the best model
- configuration, consider using
-[Keras Tuner](https://github.com/keras-team/keras-tuner).
-Note that:
-- We start the model with the `data_augmentation` preprocessor, followed by a
- `Rescaling` layer.
-- We include a `Dropout` layer before the final classification layer.
-"""
-
-def make_model(input_shape, num_classes):
-    inputs = keras.Input(shape=input_shape)
-    # Image augmentation block
-    x = data_augmentation(inputs)
-
-    # Entry block
-    x = layers.experimental.preprocessing.Rescaling(1.0 / 255)(x)
-    x = layers.Conv2D(32, 3, strides=2, padding="same")(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.Activation("relu")(x)
-
-    x = layers.Conv2D(64, 3, padding="same")(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.Activation("relu")(x)
-
-    previous_block_activation = x  # Set aside residual
-
-    for size in [128, 256, 512, 728]:
-        x = layers.Activation("relu")(x)
-        x = layers.SeparableConv2D(size, 3, padding="same")(x)
-        x = layers.BatchNormalization()(x)
-
-        x = layers.Activation("relu")(x)
-        x = layers.SeparableConv2D(size, 3, padding="same")(x)
-        x = layers.BatchNormalization()(x)
-
-        x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
-
-        # Project residual
-        residual = layers.Conv2D(size, 1, strides=2, padding="same")(
-            previous_block_activation
-        )
-        x = layers.add([x, residual])  # Add back residual
-        previous_block_activation = x  # Set aside next residual
-
-    x = layers.SeparableConv2D(1024, 3, padding="same")(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.Activation("relu")(x)
-
-    x = layers.GlobalAveragePooling2D()(x)
-    if num_classes == 2:
-        activation = "sigmoid"
-        units = 1
-    else:
-        activation = "softmax"
-        units = num_classes
-
-    x = layers.Dropout(0.5)(x)
-    outputs = layers.Dense(units, activation=activation)(x)
-    return keras.Model(inputs, outputs)
-
-parseOptions()
-if ('wmn' in opts.keys()):
-    num_classes = 2
-    if ('ncl' in opts.keys()):
-        num_classes = int(opts['ncl'])
-    model = make_model(input_shape=image_size + (3,), num_classes=num_classes)
-    model.summary()
-    model.save(opts['wmn'])
+if __name__ == '__main__':
+    parseOptions()
+    if ('wmn' in opts.keys()):
+        num_classes = 2
+        if ('ncl' in opts.keys()):
+            num_classes = int(opts['ncl'])
+        model = mic.make_model(input_shape=image_size + (3,), num_classes=num_classes, sigen=True)
+        model.summary()
+        model.save(opts['wmn'])
